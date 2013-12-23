692d39e4096b547052ca8ee88b8ea0ca707bde14
ransom <auvergnerw@gmail.com>
2013-12-23 14:17:47 +0800

    fragment template

--- DesignerTemplate/src/com/cinabit/designertemplate/FragmentActivity.java
+++ DesignerTemplate/src/com/cinabit/designertemplate/FragmentActivity.java
@@ -1,17 +1,32 @@
 package com.cinabit.designertemplate;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class FragmentActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    TextView tv = new TextView(this);
-	    tv.setText("fragment template unimplemented");
-	    setContentView(tv);
+	    setContentView(R.layout.template_fragment_activity);
+	    
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        Fragment fragment = new MyFragment();
+        fragmentTransaction.replace(R.id.frame_layout_fragment, fragment);
+        fragmentTransaction.commit();
 	}
 
+	public static class MyFragment extends Fragment {
+    	@Override
+        public View onCreateView(LayoutInflater inflater,
+        		ViewGroup container, Bundle savedInstanceState) {
+    		return inflater.inflate(R.layout.template_fragment, container, false);
+    	}
+		
+	}
 }
