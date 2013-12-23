2fc463c092c976367bfcecf3be1a65e8e6203e39
ransom <auvergnerw@gmail.com>
2013-12-23 12:52:41 +0800

    activity template

---
+++ DesignerTemplate/src/com/cinabit/designertemplate/ActivityActivity.java
@@ -0,0 +1,12 @@
+package com.cinabit.designertemplate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.template_activity);
+    }
+}
--- DesignerTemplate/src/com/cinabit/designertemplate/MainActivity.java
+++ DesignerTemplate/src/com/cinabit/designertemplate/MainActivity.java
@@ -1,15 +1,39 @@
 package com.cinabit.designertemplate;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	private Button activityButton;
+	private Button rowButton;
+	private Button dialogButton;
+	private Button fragmentButton;
+	private Button actionBarButton;
+	private Button preferencesButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.activity_main);
+
+        activityButton = (Button) findViewById(R.id.button_activity);
+        rowButton = (Button) findViewById(R.id.button_row);
+        dialogButton = (Button) findViewById(R.id.button_dialog);
+        fragmentButton = (Button) findViewById(R.id.button_fragment);
+        actionBarButton = (Button) findViewById(R.id.button_action_bar);
+        preferencesButton = (Button) findViewById(R.id.button_preferences);
+        
+        activityButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, ActivityActivity.class));				
+			}
+		});
     }
-    
+
 }
