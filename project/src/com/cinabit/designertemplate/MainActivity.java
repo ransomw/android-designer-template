fc3bddf5496b3bf12141fb0ad606638651eb2679
ransom <auvergnerw@gmail.com>
2013-12-23 12:23:51 +0800

    organized imports, start on main activity layout

--- project/src/com/cinabit/designertemplate/MainActivity.java
+++ project/src/com/cinabit/designertemplate/MainActivity.java
@@ -1,8 +1,7 @@
 package com.cinabit.designertemplate;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
